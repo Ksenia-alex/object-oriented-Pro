5498ad01514af7a0bcfc152b69799ce58d75f757
kseniatolstuhina <kt26112006@mail.ru>
2025-10-07 03:09:53 +0300

    [feat]: added the main class for running the program

---
+++ lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using object_oriented_Pro.lab1.App;
+
+namespace object_oriented_Pro.lab1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var controller = new MainController();
+        controller.Run();
+    }
+}
